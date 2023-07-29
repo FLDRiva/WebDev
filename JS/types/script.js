45a6004a7f1732697f69e34056f7a4acc7e3b295
RivaSR <westly.revin@yandex.ru>
2023-07-29 20:58:54 +0400

    Confim JS Task3

---
+++ JS/types/script.js
@@ -0,0 +1,13 @@
+let str = 'Web-developer';
+let number = 1;
+let numberSecond = 1.5;
+let bool = true;
+let undef;
+let nuull = null;
+let obj = {
+    prop: 'yo',
+    prop: function() {
+
+    }
+};
+alert(obj);
