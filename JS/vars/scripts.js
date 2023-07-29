c49f1b803b7c3ebc2b33aadf189118f8a86751ba
RivaSR <westly.revin@yandex.ru>
2023-07-29 20:19:48 +0400

    Confim JS Task2

---
+++ JS/vars/scripts.js
@@ -0,0 +1,15 @@
+'use strit';
+
+let colorEyes;
+
+let user;
+let userName = 'Вася';
+
+user = userName;
+console.log(user);
+
+/*
+Не правильные варианты обявъявления переменной:
+let user = 'Вася' let age = 36
+let myage = 36;
+*/
