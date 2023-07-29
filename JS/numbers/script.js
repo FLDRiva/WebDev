4ca1ea9ab3484dc2608bf17187c1aa9797c2b53d
RivaSR <westly.revin@yandex.ru>
2023-07-29 21:29:38 +0400

    Confim JS Task4

---
+++ JS/numbers/script.js
@@ -0,0 +1,17 @@
+// let N = new Number(4000);
+// console.log(typeof N);
+
+// console.log(N.toFixed(3));
+
+// let n = 4444;
+
+// console.log(n.toFixed(3));
+// console.log(n.toExponential(3));
+
+// console.log(5 === 5); 
+// console.log(5 !== 5); 
+// console.log(10 == '10');
+
+// console.log(Math.sqrt(81));
+// console.log(Math.pow(2, 2));
+
