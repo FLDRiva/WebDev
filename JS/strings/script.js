07adb05f22756ce1683faae57ab4e5bc49c5dc87
RivaSR <westly.revin@yandex.ru>
2023-08-05 11:42:17 +0400

    Confim Task JS string

---
+++ JS/strings/script.js
@@ -0,0 +1,30 @@
+// console.log('String');
+
+// console.log('Hello'.length);
+
+// console.log('Another \"very \\ long\" string');
+
+let string = 'Sometimes the same is different';
+console.log(string.charAt(string.length - 1));
+console.log(string.length);
+console.log(string.substring(10));
+console.log(string.substring(10,21));
+console.log(string.slice(-10));
+console.log(string.indexOf('m'));
+console.log(string.replace('is','is not'));
+console.log(string.split(' '));
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+
+console.log(string[5]);
+
+
+
+
+
+
+
+
+
+
+
