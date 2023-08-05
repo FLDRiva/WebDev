9927649bec0dce6d25036b2a97f4b033ad4abdc5
RivaSR <westly.revin@yandex.ru>
2023-08-05 12:29:59 +0400

    Confim Task JS null and undefiend

---
+++ JS/undefined/script.js
@@ -0,0 +1,19 @@
+console.log(typeof null);
+console.log(typeof undefined);
+
+let temp;
+let omj = {};
+let a = [1,2,3,4]
+
+function greet(name) {
+    return 'Hello ' + name;
+}
+
+console.log(temp);
+console.log(omj.property);
+console.log(a[4]);
+console.log(greet('Andrey'));
+console.log(greet());
+
+console.log(null == undefined);
+console.log(null === undefined);
